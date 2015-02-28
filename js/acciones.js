0d803e4e203ed3bdf082c544fb4d379dff990ea0
HugoCapitan <hmoravilez@gmail.com>
2015-02-27 21:00:01 -0600

    stuff

---
+++ js/acciones.js
@@ -0,0 +1,16 @@
+var $contenido = $('#contenido');
+var $boton = $('#boton');
+var $volver = $('#volver')
+
+function mostrarContenido() {
+	$contenido.fadeIn();
+	return false;
+}
+
+function ocultarContenido() {
+	$contenido.fadeOut();
+	return false;
+}
+
+$boton.click( mostrarContenido );
+$volver.click( ocultarContenido )
